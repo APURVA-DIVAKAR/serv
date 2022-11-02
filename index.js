2e4d34cc3b489968ef925addd86b32f89ca83d5d
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-11-02 23:55:57 +0530

    Vercel

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
+
 app.use("/api/product", product);
+app.get("/",(req, res)=>{
+
+    res.send("Hey Its Me")
+})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
