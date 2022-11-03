885600873855cd3418c9c5579fd52a1f1cbaf329
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-11-03 09:44:10 +0530

    Login Signup

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 const express = require("express");
+const UserModel = require("./model/User")
 const app = express();
-const product = require("./api/product");
+// const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
 
-app.use("/api/product", product);
+
 app.get("/",(req, res)=>{
 
-    res.send("Hey Its Me")
+    res.send("Backend Working")
 })
+
+// Signup Route
+
+app.post("/signin", async(req,res)=>{
+    const user = await new UserModel(req.body);
+    user.save((err,success)=>{
+        if(err){
+            res.status(500).send({message: "error occurred"})
+        }
+        return res.status(201).send({message: "Signed up successfully", token : 12345})
+    });
+});
+
+// Login Router
+
+app.post("/signin", async(req,res)=>{
+    // console.log(req.body);
+    const checkUser = await UserModel.findOne(req.body).lean();
+    if(checkUser){
+        let payload = {
+            ...checkUser,
+            token : 12345
+        }
+        console.log(checkUser)
+        res.send(payload);
+    }
+    else{
+        res.send({message:"Wrong Credentials"})
+    }
+});
+
+
+
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
